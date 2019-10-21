e1bfb960a1da4ce4ecf25d0392bee10c6f07aedc
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-21 13:18:45 +0800

    - use component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Button from './components/button';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,17 +10,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-    
-          <button onClick={() => 
-          { this.setState({number: this.state.number + 1})}}>
-            +
-          </button >
-  
-          <button onClick={() => 
-          { this.setState({number: this.state.number - 1})}}>
-            -
-          </button>
-          <p>number: {this.state.number}</p>
+        <header className="App-header">
+          <Button buttonApi={this.state}/>
+        </header>
       </div>
     );
   }
